5dd2e8dcf313e8a01786717249807075765cc275
LZHD <lzhdfxq@163.com>
2020-04-18 16:58:22 +0800

    build: modify build config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import ElementUI from 'element-ui';
+import { Tooltip } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ElementUI);
+Vue.use(Tooltip);
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production' ? '/vue-pictures-viewer/' : '/',
-  outputDir: 'publish'
+  outputDir: 'publish',
+  css: { extract: false },
+  productionSourceMap: false
 };
